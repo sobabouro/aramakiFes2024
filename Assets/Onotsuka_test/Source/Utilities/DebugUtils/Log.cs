ae497181778bf3a2ae9c66cdb9e426c99d148637
sobabo <kingcrimzon05097878@gmail.com>
2025-07-26 23:54:51 +0900

    add: Log.cs
    デバッグログ用のクラスを追加した

---
+++ Assets/Onotsuka_test/Source/Utilities/DebugUtils/Log.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace DebugUtils {
+
+    /// <summary>
+    /// デバッグ用のログを出力するためのユーティリティクラス
+    /// </summary>
+    public class Log {
+
+        public void PrintCollection<T>(IEnumerable<T> collection, string field) {
+            if (collection == null) {
+                Debug.Log("Collection is null");
+                return;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            System.Type type = typeof(T);
+
+            FieldInfo fieldInfo = type.GetField(field, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (fieldInfo == null) {
+                Debug.LogError($"Field '{field}' not found in type '{type.Name}'");
+                return;
+            }
+
+            foreach (var item in collection) {
+                if (item == null) {
+                    stringBuilder.AppendLine("null");
+                    continue;
+                }
+                var value = fieldInfo.GetValue(item);
+                stringBuilder.AppendLine($"{field}: {value} ");
+            }
+            Debug.Log(stringBuilder.ToString());
+        }
+    }
+}
+    
